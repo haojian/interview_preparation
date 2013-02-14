24b670f1300a9249a472fb19bb4a02c336844cd3
Haojian Jin <haojian.jin@gmail.com>
2013-02-14 14:54:20 -0800

    update

--- fisheryates_random.c
+++ fisheryates_random.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int[] shufflist = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int shufflist[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 int currentMaxIndex = -1;
 
 int getRandomNumber(currentMaxIndex){
-	int curIndex = random(currentMaxIndex);
+	int curIndex = rand()%(currentMaxIndex+1);
 	int tmp = shufflist[curIndex];
 	shufflist[curIndex] = shufflist[currentMaxIndex];
 	shufflist[currentMaxIndex] = tmp;
@@ -14,9 +14,12 @@ int getRandomNumber(currentMaxIndex){
 
 int main(){
 	int length = sizeof(shufflist)/sizeof(shufflist[0]);
+	printf("array length: %d\n", length);
+	
 	currentMaxIndex = length-1;
-	while(currentMaxIndex > -1){
+	while(currentMaxIndex >= 0){
 		printf("%d\t", getRandomNumber(currentMaxIndex));
 		currentMaxIndex--;
 	}
+	printf("\n");
 }
