a62b4f6285fe1f3d346059bbe0489dda91b2c9f3
Haojian Jin <haojian.jin@gmail.com>
2013-02-15 14:35:07 -0800

    update

--- commonancestor.c
+++ commonancestor.c
@@ -3,19 +3,30 @@ Find an ancestor of given two node from a tree in O(n) time. The tree is binary 
 *****/
 
 #include <stdio.h>
+#include <vector.h>
 
 typedef struct _node{
-	
+	int val;
+	struct _node *leftChild;
+	struct _node *rightChild;
 }node;
+
 
 node* commonAncestor(node *nodeA, node *nodeB, node *root){
 
 }
 
-int initBTree(node *root){
+int initBTree(node *node){
 	
+	
+	(*root)->val = 100;
+	(*root)->leftChild = 
+	(*root)->rightChild = 
+	for(int i =0 ;i < 100; )
 }
 
 int main(){
-		
+	node *root = malloc(sizeof(node));
+	
+	
 }
---
+++ lru.c
@@ -0,0 +1,3 @@
+/*
+Considering the multi-thread problem, design a LRU cache mechanism based on double linked list & hashmap.
+*/
