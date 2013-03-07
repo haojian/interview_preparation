005d604c88b094ceb7295edbaca5131208ce8ae6
Haojian Jin <haojian.jin@gmail.com>
2013-03-06 17:22:25 -0800

    update

---
+++ crosswordpuzzle.c
@@ -0,0 +1,5 @@
+/****
+I was asked to write a routine to verify that all the words in the solution of a crossword puzzle were valid words. 
+The crossword puzzle was represented as a two-dimensional array of single-character strings (and empty strings for the empty blocks).
+
+****/
