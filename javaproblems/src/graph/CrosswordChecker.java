662e70b52a03c422422fd5ee68ad15bc272deab5
Haojian Jin <haojian.jin@gmail.com>
2013-03-07 18:03:06 -0800

    UPDATE

---
+++ javaproblems/src/graph/CrosswordChecker.java
@@ -0,0 +1,19 @@
+package graph;
+
+public class CrosswordChecker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public boolean check(String grid[][]){
+		int height = grid.length;
+		int width = grid[0].length;
+		return false;
+	}
+}
