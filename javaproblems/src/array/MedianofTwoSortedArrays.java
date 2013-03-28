b45f213a7f824c90e323f8bf09b8f3b8cee4e866
Haojian Jin <haojian.jin@gmail.com>
2013-03-27 18:45:04 -0700

    update

--- javaproblems/src/array/MedianofTwoSortedArrays.java
+++ javaproblems/src/array/MedianofTwoSortedArrays.java
@@ -7,7 +7,21 @@ public class MedianofTwoSortedArrays {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int A[] = {1, 2, 3, 4, 5, 6, 7};
+	}
+	
+	public int findMedianofTwoSortedArrays(int A[], int B[]){
+		int rangeA[] = new int[2];
+		int rangeB[] = new int[2];
+		int k = (A.length+B.length-1)/2;
+ 		if((A.length + B.length)%2 == 1){
+			return (findKthInSortedArrays(A, B, rangeA, rangeB, k)  + findKthInSortedArrays(A, B, rangeA, rangeB, k+1))/2;
+		}else{
+			return findKthInSortedArrays(A, B, rangeA, rangeB, k);
+		}
 	}
 
+	public int findKthInSortedArrays(int A[], int B[], int rangeA[], int rangeB[], int k){
+		return -1;
+	}
 }
