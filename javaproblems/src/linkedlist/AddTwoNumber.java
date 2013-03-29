ed75a9e5904900fafa1d4d4f3656804aa42c7643
Haojian Jin <haojian.jin@gmail.com>
2013-03-28 18:10:25 -0700

    update

---
+++ javaproblems/src/linkedlist/AddTwoNumber.java
@@ -0,0 +1,59 @@
+package linkedlist;
+
+
+public class AddTwoNumber {
+
+	class ListNode {
+	    public int val;
+	    public ListNode next;
+	    ListNode(int x) {
+	        val = x;
+	        next = null;
+	    }
+}
+	/**
+	  @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode res = null;
+        ListNode end = null;
+        int update = 0;
+        int val1 = 0;
+        int val2 = 0;
+        while(l1 != null || l2!= null || update != 0){
+        	if(l1 != null){
+        		val1 = l1.val;
+                l1 = l1.next;
+        	}
+        	else
+        		val1 = 0;
+        	if(l2 != null){
+        		val2 = l2.val;
+        		l2 = l2.next;
+        	}
+        	else
+        		val2 = 0;
+            int digit = val1 + val2 + update;
+            update = 0;
+            if(digit>=10){
+                update = 1;
+                digit -= 10;
+            }
+            if(res == null){
+            	res = new ListNode(digit);
+            	end = res;
+            }else{
+            	end.next = new ListNode(digit);
+            	end = end.next;
+            }
+        }
+        return res;
+    }
+}
