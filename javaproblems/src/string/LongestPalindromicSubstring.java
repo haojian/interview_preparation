6a7d8f54e9dee62c027408b1058243c34bf6c916
Haojian Jin <haojian.jin@gmail.com>
2013-03-28 18:28:01 -0700

    update

---
+++ javaproblems/src/string/LongestPalindromicSubstring.java
@@ -0,0 +1,21 @@
+package string;
+
+
+/*Given a string S, find the longest palindromic substring in S. 
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ * */
+
+public class LongestPalindromicSubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "bananas";
+		System.out.println(longestPalindrome(test));
+	} 
+	
+    public static String longestPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        return null;
+    }
+}
