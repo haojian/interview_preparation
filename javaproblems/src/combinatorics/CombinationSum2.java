255f54a6831745d6688a22ed6b876d27c2b16361
Haojian Jin <haojian.jin@gmail.com>
2013-04-15 17:41:28 -0700

    update

--- javaproblems/src/combinatorics/CombinationSum2.java
+++ javaproblems/src/combinatorics/CombinationSum2.java
@@ -1,8 +1,26 @@
 package combinatorics;
 
+import java.util.ArrayList;
+
 public class CombinationSum2 {
 
 	/**
+	 * 
+	 * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, , ak) must be in non-descending order. (ie, a1 ? a2 ?  ? ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
+A solution set is: 
+[1, 7] 
+[1, 2, 5] 
+[2, 6] 
+[1, 1, 6] 
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -10,4 +28,10 @@ public class CombinationSum2 {
 
 	}
 
+	
+	public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        return null;
+    }
 }
