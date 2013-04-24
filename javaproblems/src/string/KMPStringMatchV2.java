854dac85fc3bf5e36206e8687af77d61236e1b8e
Haojian Jin <haojian.jin@gmail.com>
2013-04-23 17:32:45 -0700

    update

--- javaproblems/src/string/KMPStringMatchV2.java
+++ javaproblems/src/string/KMPStringMatchV2.java
@@ -3,9 +3,13 @@ package string;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class KMPStringMatcher {
+public class KMPStringMatchV2 {
 
 	/**
+	 * 
+	 * Example for understanding "baabdbaabe"
+	 * 
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -14,23 +18,20 @@ public class KMPStringMatcher {
 		System.out.println(strStr("mississippi", "issip"));
 	}
 	
-	public static String strStr(String haystack, String needle) {
+    public static String strStr(String haystack, String needle) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(needle == null || needle.length() == 0)
-        	return haystack;
-        if(haystack == null)
-        	return null;
         int hi = 0;
         int ni = 0;
         int[] next = getNext(needle);
+
         while(hi<haystack.length() && ni<needle.length()){
         	if(haystack.charAt(hi) == needle.charAt(ni)){
         		hi++;
         		ni++;
-        	}	
+        	}
         	else{
-        		if(ni == 0)
+        		if(ni==0)
         			hi++;
         		else
         			ni = next[ni-1]+1;
@@ -38,25 +39,22 @@ public class KMPStringMatcher {
         }
         if(hi == haystack.length())
         	return null;
-
-        
-        else
-        	return haystack.substring(hi-needle.length());
-    }
+        return haystack.substring(hi-needle.length());
+    }                                                                                                                                                
     
     public static int[] getNext(String pattern){
-    	int[] next =new int[pattern.length()];
-    	next[0] = -1;
-    	for(int i=1; i<next.length; i++){
-    		int k = next[i-1];
-    		while(pattern.charAt(k+1) != pattern.charAt(i) && k>=0){
-    			k = next[k];
-    		}
-    		if(pattern.charAt(i) == pattern.charAt(k+1))
-    			next[i] = k +1;
-    		else
-    			next[i] = -1;
-    	}
-    	return next;
+    	int[] res = new int[pattern.length()];
+		res[0] = -1;
+		for(int i=1; i<pattern.length(); i++){
+			int k = res[i-1];
+			while(pattern.charAt(i) != pattern.charAt(k+1) && k>=0){
+				k = res[k];
+			}
+			if(pattern.charAt(i) == pattern.charAt(k+1))
+				res[i] = k+1;
+			else
+				res[i] = -1;
+		}
+		return res;
     }
 }
