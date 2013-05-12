a426ca0ca3226e93820422534f749a9d2569c080
Haojian Jin <haojian.jin@gmail.com>
2013-05-12 12:55:13 -0700

    update

---
+++ javaproblems/src/twosigma/FileCpy.java
@@ -0,0 +1,74 @@
+package twosigma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileCpy {
+
+	/**
+	 * Since we assume the input will be valid, I didn't do data validation in this program, which is easy to add in the future.
+	 * 
+	 * Test cases:
+	 * 	duplicate data files in one line.
+	 * 	empty line.
+	 *  boundary dataset id value
+	 *  line# = 0.
+	 *  
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String line = br.readLine();
+			int n =Integer.parseInt(line);
+			FileCpy  fc= new FileCpy();
+			int i=0;
+			while( i<n && (line = br.readLine()) != null){
+				i++;
+				fc.processLine(line, i);
+			}
+			fc.copyFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Map<String, Integer> filemap;	//store all the unique DataSet names
+	private Map<Integer, Map<String, Integer>> dcentermap;		//store the existing files on different Data Center. HashMap for O(1) lookup operation.
+	
+	public FileCpy(){
+		filemap = new HashMap<String, Integer>();
+		dcentermap = new HashMap<Integer, Map<String, Integer>>();
+	}
+	
+	//update value in the two maps.
+	public void processLine(String line, int n){
+		String[] files = line.split(" ");
+		dcentermap.put(n, new HashMap<String, Integer>());
+		for(int i=1; i<files.length;i++){
+			if(!filemap.containsKey(files[i]))
+				filemap.put(files[i], n);
+			dcentermap.get(n).put(files[i], 0);
+		}
+	}
+	
+	//traverse all the data center to check if any file is missing.
+	public void copyFile(){
+		for(Map.Entry<Integer, Map<String, Integer>> dCenter: dcentermap.entrySet()){
+			for(Map.Entry<String, Integer> entry:filemap.entrySet()){
+				if(!dCenter.getValue().containsKey(entry.getKey())){
+					System.out.println(entry.getKey() +" " + entry.getValue() + " " + dCenter.getKey());
+				}
+			}
+		}
+		System.out.println("done");
+	}
+}
---
+++ javaproblems/src/twosigma/JumbleSort.java
@@ -0,0 +1,89 @@
+package twosigma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JumbleSort {
+
+	/**
+	 * Since we assume the input will be valid, 
+	 * I didn't do data validation in this program, 
+	 * which is easy to add in the future.
+	 * 
+	 * Test cases:
+		car truck 8 4 bus 6 1
+		car truck -999999 4 bus 6 999999     6  6   //illegal input. 
+		car truck -999999 4 bus 6 999999 6 6
+		-999999 
+		dcar truck -81 -141 bus 623 100
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		JumbleSort js = new JumbleSort();
+		try {
+			String line = br.readLine();
+			ArrayList<String> res = js.processLine(line);
+			StringBuilder output = new StringBuilder();
+			for(String single: res){
+				output.append(single);
+				output.append(" ");
+			}
+			System.out.println(output.toString().trim());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	//Words will not contain spaces
+	public ArrayList<String>  processLine(String line){
+		if(line == null || line.length() == 0){
+			return new ArrayList<String>();
+		}
+		String[] subStrs = line.split(" ");
+		return sort(subStrs);
+	}
+	
+	
+	//split the input into two parts "words" + "numbers", and sort them independently. 
+	//then merge them based on "boolean[] flag"
+	public ArrayList<String> sort(String[] input){
+		ArrayList<String> res = new ArrayList<String>();
+		if(input == null || input.length == 0){
+			return res;
+		}
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+    	ArrayList<String> words = new ArrayList<String>();
+    	boolean[] flag = new boolean[input.length];
+    	for(int i=0; i<input.length; i++){
+    		try {
+    		    int num = Integer.parseInt(input[i]);
+    		    numbers.add(num);
+    		    flag[i] = true;
+    		} catch(NumberFormatException e) {
+    			words.add(input[i]);
+    			flag[i] = false;
+    		}
+    	}
+    	Collections.sort(numbers);
+    	Collections.sort(words);
+    	int numi=0;
+    	int wordi=0;
+    	for(int i=0; i<input.length; i++){
+    		if(flag[i]){
+    			res.add(Integer.toString(numbers.get(numi)));
+    			numi++;
+    		}else{
+    			res.add(words.get(wordi));
+    			wordi++;
+    		}
+    	}
+		return res;
+	}
+}
