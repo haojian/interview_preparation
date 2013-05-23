30a004d4512fd6eaf4687aa3252ff5938b2f434d
Haojian Jin <haojian.jin@gmail.com>
2013-05-23 12:03:57 -0700

    update

---
+++ InterviewProblems/src/mathprob/RandomlyReadLine.java
@@ -0,0 +1,36 @@
+package mathprob;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Random;
+
+public class RandomlyReadLine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String randLine(String path){
+		String res = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = "";
+			Random rand = new Random();
+			int counter = 0;
+			while((line = br.readLine()) != null){
+				if(rand.nextInt(counter+1) == 0 )
+					res = line;
+				counter++;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
+}
