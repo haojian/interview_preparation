8e0bc40e21fd7d2f3a86ea10d6f6b2f2443193d3
haojian jin <haojian.jin@gmail.com>
2013-05-24 13:08:25 -0700

    update

--- Careercup/src/three/MinStack.java
+++ Careercup/src/three/MinStack.java
@@ -8,6 +8,7 @@ public class MinStack extends Stack<Integer>{
 	 * 3 points:
 	 * 	1. push  i<= min(); also need to be equal for all the possible cases
  	 *  2. Use min rather than peek. Since the minStack could be empty;
+ 	 *  
 	 * @param args
 	 */
 	public static void main(String[] args) {
